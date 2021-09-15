589aa591664605d4a56b1b039189ced72bfef630
gogsi <dragutinovic.g@gmail.com>
2021-09-14 21:53:42 -0700

    "Committing Project as One Directory"

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Dividend Screener</h1>
+      <nav>
+        <ul>
+          <li>
+            About
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
