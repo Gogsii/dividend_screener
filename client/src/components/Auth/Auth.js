589cd5e5ab9894a0c65f4d829b7931bb12681637
gogsi <dragutinovic.g@gmail.com>
2021-09-29 17:08:04 -0700

    "Added logging in and authentication"

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import useStyles from './styles';
+import Input from './Input';
+import Icon from './icon';
+
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false); //setting default case to be not signed up
+
+    const handleShowPassword = () => setShowPassword( (prevShowPassword) => !prevShowPassword) //this toggles the show pw on/off
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const switchMode = () => {
+        (setIsSignup( (prevIsSignup) => !prevIsSignup )); //switches the state on/off like a switch
+        handleShowPassword(false); //resets the show password when switching between signup/signin
+    };  
+
+    const googleSuccess = async (res) => {
+        //special optional chaining operator ?. is not going to throw an error if we don't have access to the res object
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+        
+        try {
+            dispatch({type: 'AUTH', data: { result, token } }); //dispatching action type and payload
+            history.push('/'); //navigate user back to home after logging in
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(res);
+    };
+
+    //if sign up with google fails, return why
+    const googleFailure = (error) => {
+        console.log(error);
+        console.log('Google Sign In was unsuccessful. Try Again Later.')
+    };
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container component='main' maxWidth='xs'>
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant='h5'>{ isSignup ? 'Sign Up' : 'Sign In' }</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        { isSignup && (
+                        <>
+                            <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half/>
+                            <Input name='lastName' label='Last Name' handleChange={handleChange} half/>
+                        </>
+                        )}
+                        <Input name='email' label='Email Address' type='email' handleChange={handleChange} />
+                        <Input name='password' label='Password' type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} handleChange={handleChange}/>
+                        { isSignup && <Input name='confirmPassword' label='Repeat Password' type='password' handleChange={handleChange}/> }
+                    </Grid>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                        { isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <GoogleLogin 
+                        clientId='941971253122-02s9drkrmvukt8fha77buslca1u4g40u.apps.googleusercontent.com'
+                        render={(renderProps) => (
+                            <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant='contained' >
+                                Google Sign In
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy='single_host_origin'
+                    />
+                    <Grid container justify='flex-end'>
+                        <Button onClick={switchMode}>
+                            {isSignup ? 'Already have an account? Sign In' : 'Don\'t have an account? Sign Up'}
+                        </Button>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,28 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-import dividendstocks from '../../images/dividendstocks.jpg';
-
+import { useDispatch } from 'react-redux';
+//import dividendstocks from '../../images/dividendstocks.jpg';
+import authReducer from '../../reducers/auth';
 
 import useStyles from './styles';
 
 const Navbar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile'))); //here we attempt to fetch the user from local storage
+    
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+
+        history.push('/');
+
+        setUser(null);
+    };
+
+    //using use effect to help navigate automatically instead of manually
+    useEffect(() => {
+        const token = user?.token;
+        // JWT... later on
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
     return(
     <AppBar className={ classes.appBar } position='static' color='inherit'>
         <div className={classes.brandContainer}>
             <Typography component={Link} to='/' className={ classes.heading } variant='h2' align='center'>Dividend Stock Screener</Typography>
-            <img className={ classes.image } src={ dividendstocks } alt='logo' height='220' />
+           {/* <img className={ classes.image } src={ dividendstocks } alt='logo' height='220' /> */}
         </div>
         <Toolbar className={classes.toolbar}>
             {user ? (
                 <div className={classes.profile}>
                     <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                     <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                    <Button variant='contained' className={classes.logout}>Logout</Button>
+                    <Button variant='contained' className={classes.logout} onClick={logout}>Logout</Button>
                 </div>
             ) : (
                 <Button component={Link} to='/auth' variant='contained' color='primary'>Log In</Button>
