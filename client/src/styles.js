0075b6b3ef819611a1129f567fc3025d1b3b0813
gogsi <dragutinovic.g@gmail.com>
2021-10-05 17:27:34 -0700

    "Adding search functionality"

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: 'column-reverse',
+    },
+  },
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -13,25 +18,44 @@ export default makeStyles((theme) => ({
     color: 'rgba(0,183,255, 1)',
   },
   image: {
-    margin: '5px',
-    paddingLeft: '30px',
-    width: '40%',
+    marginLeft: '15px',
   },
-
-  [theme.breakpoints.down('sm')]: {
-    mainContainer: {
-      flexDirection: "column-reverse"
-    },
-    image: {
-      margin: '0',
-      width: '100%',
-      borderRadius: '10px',
-    },
-    appBar: { 
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-     }
-    }
-
 }));
+
+// import { makeStyles } from '@material-ui/core/styles';
+
+// export default makeStyles((theme) => ({
+//   appBar: {
+//     borderRadius: 15,
+//     margin: '30px 0',
+//     display: 'flex',
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   heading: {
+//     color: 'rgba(0,183,255, 1)',
+//   },
+//   image: {
+//     margin: '5px',
+//     paddingLeft: '30px',
+//     width: '40%',
+//   },
+
+//   [theme.breakpoints.down('sm')]: {
+//     mainContainer: {
+//       flexDirection: "column-reverse"
+//     },
+//     image: {
+//       margin: '0',
+//       width: '100%',
+//       borderRadius: '10px',
+//     },
+//     appBar: { 
+//       display: 'flex',
+//       flexDirection: 'column',
+//       justifyContent: 'center',
+//      }
+//     }
+
+// }));
