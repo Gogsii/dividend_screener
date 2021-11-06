d9c7de436d775960239ad100509d90ee8e9389b7
gogsi <dragutinovic.g@gmail.com>
2021-11-05 17:36:49 -0700

    "Updated design and search stock by ticker API"

--- client/src/components/Stocks/Stocks.js
+++ client/src/components/Stocks/Stocks.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux'; //lets us tap into the store
 import Stock from './Stock/Stock';
 import StockSearch from './Stock/StockSearch';
